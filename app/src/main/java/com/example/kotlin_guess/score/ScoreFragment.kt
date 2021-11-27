d657a91b305a374175738ae5c8908a8e994aa8ec
unknown <giomozda74@gmail.com>
2021-11-27 20:25:42 +0200

    starter code

---
+++ app/src/main/java/com/example/kotlin_guess/score/ScoreFragment.kt
@@ -0,0 +1,35 @@
+package com.example.kotlin_guess.score
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.kotlin_guess.R
+import com.example.kotlin_guess.databinding.ScoreFragmentBinding
+
+class ScoreFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.score_fragment, container, false
+        )
+
+        // Get args using by navArgs property delegate
+        val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
+        binding.scoreText.text = scoreFragmentArgs.score.toString()
+        binding.playAgainButton.setOnClickListener { onPlayAgain() }
+        return binding.root
+    }
+
+    private fun onPlayAgain() {
+        findNavController().navigate(ScoreFragmentDirections.actionRestart())
+    }
+}
